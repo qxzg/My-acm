5dd25199bd75954b3c82eab4dc1eafa62efad126
qxzg <out@qxzg.xyz>
2019-11-30 12:18:37 +0800

    1031

--- luogu/P1031.cpp
+++ luogu/P1031.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int a[101] = {};
-	int n,i,m = 0,p = 0;
-	cin>>n;
-	for(i = 0; i < n; i++)
+	int n, i, m = 0, p = 0;
+	cin >> n;
+	for (i = 0; i < n; i++)
 	{
-		cin>>a[i];
+		cin >> a[i];
 		p += a[i];
 	}
 	p /= n;
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		a[i] -= p;
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if(a[i] == 0)
+		if (a[i] == 0)
 			continue;
-		a[i+1] += a[i];
+		a[i + 1] += a[i];
 		m++;
 	}
-	cout<<m;
+	cout << m;
 	return 0;
 }
-
